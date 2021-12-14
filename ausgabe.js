89e23947293d09743b22fed4923d43d20d357c4d
Giulia Laugel <laugelg@bzz.ch>
2021-12-14 17:15:37 +0100

    produkt seite mit JavaScript am erstellen + 2 JavaScript Seiten erstellt

---
+++ ausgabe.js
@@ -0,0 +1,10 @@
+let product = undefined;
+
+function showUI()
+{
+    let htmlObj = document.getElementById(elementId:"produktbeschreibung");
+    htmlObj.innerHTML=
+        '${produkt.produktName<br>'+
+        '${produkt.produktPreis<br>'+
+        '${produkt.produktbeschreibung<br>'+
+}
