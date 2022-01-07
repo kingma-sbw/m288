24dd2535566c8dc554cba404ffec6f6d20462525
Giulia Laugel <laugelg@bzz.ch>
2022-01-07 22:15:53 +0100

    nun ist javaScript hinzugefügt auf Github

---
+++ ausgabe.js
@@ -0,0 +1,76 @@
+/* Deklaration der Variabeln */
+let index;
+let produkt = undefined;
+
+/* Ausgabe */
+function showUI() {
+    //Produktname
+    let htmlObj = document.getElementById("produktName");
+    htmlObj.innerHTML =
+        `${produkt.name}`;
+
+    //Preis des Produktes
+        htmlObj = document.getElementById("produktPreis");
+        htmlObj.innerHTML =
+            `CHF ${produkt.price}`;
+
+    //Bild des Produktes
+        htmlObj = document.getElementById("produktImage");
+        htmlObj.src=
+            `${produkt.imageFile}`;
+
+    //Beschreibung des Produktes
+        htmlObj = document.getElementById("produktText");
+        htmlObj.innerHTML =
+            `${produkt.beschreibung}`;
+    }
+
+// nächstes Produkt
+    function next() {
+        //Browser speicher hinauslesen
+        index = localStorage.getItem("index");
+        //testausgabe
+        console.log(index);
+        if (index == undefined) {
+            index = 0;
+        } else {
+            if (produktListe.length - 1 > index) {
+                index++; //Array wird erhöht
+            }
+        }
+        //Speicherung im Index Browser
+        localStorage.setItem("index", index);
+        produkt = produktListe[index];
+        //zeige es
+        showUI();
+    }
+
+// vorheriges Produkt
+    function back() {
+        //Browser speicher hinauslesen
+        index = localStorage.getItem("index");
+        //testausgabe
+        console.log(index);
+        if (index == undefined) {
+            index = 0;
+        } else {
+            if (index > 0) {
+                index--; //Array wird verkleinert
+            }
+        }
+        //Speicherung im Index Browser
+        localStorage.setItem("index", index);
+        produkt = produktListe[index];
+        //zeige es
+        showUI();
+    }
+
+//Falls der index nicht definiert ist ...
+if (produkt === undefined){
+    index = 0;
+    localStorage.setItem("index", index);
+    //letzte Anzahl holen
+    produkt = produktListe[index];
+    //zeige den Eintrag
+    showUI();
+}
---
+++ data.js
@@ -0,0 +1,34 @@
+/* JSON */
+let produktListe =
+[
+    {
+     "id": 1,
+    "name": "beer1",
+    "price": "10.00",
+    "imageFile":"grafiken/produkt_bilder/brewed_ginger_beer.png",
+    "beschreibung": "text",
+    "svgFile":"icons/icon1.svg"
+    },{
+    "id": 2,
+    "name": "beer2",
+    "price": "9.00",
+    "imageFile":"grafiken/produkt_bilder/edelflower_tonic_water.png",
+    "beschreibung": "text2",
+    "svgFile":"icons/icon1.svg"
+    },{
+    "id": 3,
+    "name": "beer3",
+    "price": "12.00",
+    "imageFile":"image/bild3.JPG",
+    "beschreibung": "text3",
+    "svgFile":"icons/icon1.svg"
+    },{
+    "id": 4,
+    "name": "beer4",
+    "price": "13.00",
+    "imageFile":"image/bild4.JPG",
+    "beschreibung": "text4",
+    "svgFile":"icons/icon1.svg"
+    }
+]
+
